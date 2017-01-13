c92ff2dedb4ca323f661d1abf4d8fd6831bba5ad
Rukia Sheikh-mohamed <rukiasheikh-mohamed@macbook-2.local>
2017-01-12 20:33:38 -0600

    corrections

--- app.js
+++ app.js
@@ -3,35 +3,35 @@ $(function () {
 
 
 
-$('button').on('click', function () {
-     $('#boxX').append('<div class="square"><button class="delete">X</button></div>');
-     $('.square:last').css('background-color', randomNumber());
-     console.log("Yay!You made a square!");
+  $('button').on('click', function () {
+    $('#boxX').append('<div class="square"><button class="delete">X</button></div>');
+    $('.square:last').css('background-color', randomNumber());
+    console.log("Yay!You made a square!");
 
-     //:last finds the last div with a class of square that was appended to DOM
+    //:last finds the last div with a class of square that was appended to DOM
   });
-$('#boxX').on('click', '.square', function (){
-  $(this).css('background-color', 'black');
-  console.log('Square is now black');
-  //look in body on click in the square div and turn the box black
-});
-// $('body').on('click', '.square' ,function (){
-//   $(this).css('background-color', randomNumber());
-//   console.log("Now it's a random color!Cool Beans.");
-//
-// });
-$('#boxX').on('click', '.delete', function () {
+  $('#boxX').on('click', '.square', function (){
+    $(this).css('background-color', 'black');
+    // console.log('Square is now black');
+    //look in body on click in the square div and turn the box black
+  });
+  // $('body').on('click', '.square' ,function (){
+  //   $(this).css('background-color', randomNumber());
+  //   console.log("Now it's a random color!Cool Beans.");
+  //
+  // });
+  $('#boxX').on('click', '.delete', function () {
     $(this).parent().remove();
   });
 
 });
 function randomNumber() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++ ) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++ ) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 
 
 }
